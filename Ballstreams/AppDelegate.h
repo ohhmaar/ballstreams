84746935103da6c576ef4e7f05a33cb790735564
Omar Basrawi <omarbasrawi@gmail.com>
2014-11-20 16:21:42 -0600

    Initial Commit

---
+++ Ballstreams/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ballstreams
+//
+//  Created by Omar Basrawi on 11/20/14.
+//  Copyright (c) 2014 Omar Basrawi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
